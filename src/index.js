b1c3e5aba466eaa0c52b7db71abe5a26234d9efb
unknown <pridanov.ilya@mail.ru>
2020-09-30 22:15:47 +0400

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        let numberMin = array[0];
+        array.forEach(function(item) {
+            if (item < numberMin) {
+                numberMin = item;
+            }
+        });
+        return numberMin;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        let numberMax = array[0];
+        array.forEach(function(item) {
+            if (item > numberMax) {
+                numberMax = item;
+            }
+        });
+        return numberMax;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        array.forEach(function(item) {
+            sum += item;
+        });
+        let number = sum / array.length;
+        return number;
+    }
 }
+
